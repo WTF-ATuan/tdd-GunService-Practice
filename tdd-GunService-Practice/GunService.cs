d42aa3a88850bba9581ff8cc1dd212a8e663327a
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2022-10-11 16:21:23 +0800

    create Gun service and Pass first test "fire_when_7_bullet_then_should_return_6_bullet"

---
+++ tdd-GunService-Practice/GunService.cs
@@ -0,0 +1,16 @@
+﻿namespace tdd_GunService_Practice;
+
+public class GunService{
+	private int _currentAmmoCount = 0;
+
+	public GunService(int maxAmmoCount){
+		_currentAmmoCount = maxAmmoCount;
+	}
+	public void Fire(){
+		_currentAmmoCount -= 1;
+		OnFire.Invoke(_currentAmmoCount);
+	}
+
+	public Action<int> OnFire{ get; set; }
+	public Action NoAmmo{ get; set; }
+}
--- tdd-GunService-Practice/GunServiceTests.cs
+++ tdd-GunService-Practice/GunServiceTests.cs
@@ -1,7 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
 
 namespace tdd_GunService_Practice;
 
+[SuppressMessage("Assertion",
+	"NUnit2005:Consider using Assert.That(actual, Is.EqualTo(expected)) instead of Assert.AreEqual(expected, actual)")]
 public class GunServiceTests{
+	private GunService _gunService;
+
+	[SetUp]
+	public void Setup(){
+		_gunService = new GunService(7);
+	}
+	[Test]
+	public void fire_when_7_bullet_then_should_return_6_bullet(){
+		var gunService = _gunService;
+		var returnBullet = 0;
+
+		void TestAction(int amount){
+			returnBullet = amount;
+		}
+
+		gunService.OnFire += TestAction;
+		gunService.Fire();
+		Assert.AreEqual(6, returnBullet);
+	}
 	
 }
